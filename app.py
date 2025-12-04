d3631c66de93b476bcdea6394f564555ce4e091f
aisyahis13 <163330409+aisyahis13@users.noreply.github.com>
2025-12-04 17:24:44 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    waktu = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"""
+    <div style="text-align: center; margin-top: 50px; font-family: Arial;">
+        <h1>Tugas 12 - PaaS (Render)</h1>
+        <h3>Kelompok: [Tulis Nama Kelompokmu Disini]</h3>
+        <p>Aplikasi ini berjalan di atas <strong>Render (PaaS)</strong></p>
+        <hr>
+        <p>Waktu Server: {waktu}</p>
+    </div>
+    """
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
